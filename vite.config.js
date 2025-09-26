abde11ac2f1a80cc699cd410210ee5ab233aca5e
Yash Maheshwari <maheshwariy930@gmail.com>
2025-09-27 02:55:40 +0530

    Correcting backend error

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// Detect environment (development or production)
+const isProd = process.env.NODE_ENV === "production";
+
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
+  server: !isProd
+    ? {
+        proxy: {
+          "/api": {
+            target: "http://localhost:5000", // Local backend
+            changeOrigin: true,
+            rewrite: (path) => path.replace(/^\/api/, ""), // Keep for dev
+          },
+        },
+      }
+    : {},
 });
